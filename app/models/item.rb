8bb5af91d33a68d438b849028167b07fcdc468c3
saya4444 <smr.viola.38@gmail.com>
2024-02-17 17:28:15 +0900

    バリデーション　追加
    アクティブハッシュベース　
    ---の場合保存不可になるよう設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,11 +11,11 @@ class Item < ApplicationRecord
   validates :image        , presence: true
   validates :item         , presence: true
   validates :explanation  , presence: true
-  validates :category_id  , presence: true
-  validates :condition_id , presence: true
-  validates :burden_id    , presence: true
-  validates :pref_id      , presence: true
-  validates :period_id    , presence: true
+  validates :category_id  , presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id , presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :burden_id    , presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :pref_id      , presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :period_id    , presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :price        , presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, format: { with: /\A[0-9]+\z/ }
   validates :user         , presence: true
 
