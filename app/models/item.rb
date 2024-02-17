79d98dc4b4e0e173b005e70784597d4da3c2e885
saya4444 <smr.viola.38@gmail.com>
2024-02-17 19:19:37 +0900

    バリデーション　修正
    エラーハンドリング
    エラー文重複解消

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,16 +8,18 @@ class Item < ApplicationRecord
     belongs_to_active_hash :period
 
 
-  validates :image        , presence: true
-  validates :item         , presence: true
-  validates :explanation  , presence: true
-  validates :category_id  , presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :condition_id , presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :burden_id    , presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :pref_id      , presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :period_id    , presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :price        , presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, format: { with: /\A[0-9]+\z/ }
-  validates :user         , presence: true
+validates :image        , presence: true
+validates :item         , presence: true
+validates :explanation  , presence: true
+validates :category_id  , presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+validates :condition_id , presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+validates :burden_id    , presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+validates :pref_id      , presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+validates :period_id    , presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+validates :price        , presence: true, presence: { message: "can't be blank" },
+                                          numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "must be a number between 300 and 9999999" }, 
+                                          format: { with: /\A[0-9]+\z/ }
+validates :user         , presence: true
 
 
   belongs_to       :user
