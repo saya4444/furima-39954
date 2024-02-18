cabffedc0b4ac424321249f07852858b844ffb24
saya4444 <smr.viola.38@gmail.com>
2024-02-18 16:39:25 +0900

    レビュー　修正対応
    バリデーションの修正
    コントローラーの修正
    エラーメッセージの日本語の取りやめ
    テストコード修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,8 +16,7 @@ validates :condition_id , presence: true, numericality: { other_than: 1 }
 validates :burden_id    , presence: true, numericality: { other_than: 1 }
 validates :pref_id      , presence: true, numericality: { other_than: 1 }
 validates :period_id    , presence: true, numericality: { other_than: 1 }
-validates :price        , presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, format: { with: /\A[0-9]+\z/ }
-validates :user         , presence: true
+validates :price        , presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 
 
   belongs_to       :user
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Item, type: :model do
 
   before do
     @item = FactoryBot.build(:item)
-    @item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
   end
 
   describe '商品出品機能' do
@@ -19,109 +18,109 @@ RSpec.describe Item, type: :model do
       it 'imageが空では保存できない' do
         @item.image = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("商品画像 が空では保存できません")
+        expect(@item.errors.full_messages).to include("Image can't be blank")
       end
 
       it 'itemが空では保存できない' do
         @item.item = ""
         @item.valid?
-        expect(@item.errors.full_messages).to include("商品名 が空では保存できません")
+        expect(@item.errors.full_messages).to include("Item can't be blank")
       end
 
       it 'explanationが空では保存できない' do
         @item.explanation = ""
         @item.valid?
-        expect(@item.errors.full_messages).to include("商品の説明 が空では保存できません")
+        expect(@item.errors.full_messages).to include("Explanation can't be blank")
       end
 
       it 'category_idが空では保存できない' do
         @item.category_id = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("カテゴリー が空では保存できません")
+        expect(@item.errors.full_messages).to include("Category can't be blank")
       end
 
       it 'category_idが1では保存できない' do
         @item.category_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("カテゴリー は1以外の値にしてください")
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
       end
 
       it 'condition_idが空では保存できない' do
         @item.condition_id = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("商品の状態 が空では保存できません")
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
       end
 
       it 'condition_idが1では保存できない' do
         @item.condition_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("商品の状態 は1以外の値にしてください")
+        expect(@item.errors.full_messages).to include("Condition must be other than 1")
       end
 
       it 'burden_idが空では保存できない' do
         @item.burden_id = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("配送料の負担 が空では保存できません")
+        expect(@item.errors.full_messages).to include("Burden can't be blank")
       end
 
       it 'burden_idが1では保存できない' do
         @item.burden_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("配送料の負担 は1以外の値にしてください")
+        expect(@item.errors.full_messages).to include("Burden must be other than 1")
       end
 
       it 'pref_idが空では保存できない' do
         @item.pref_id = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("発送元の地域 が空では保存できません")
+        expect(@item.errors.full_messages).to include("Pref can't be blank")
       end
 
       it 'pref_idが1では保存できない' do
         @item.pref_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("発送元の地域 は1以外の値にしてください")
+        expect(@item.errors.full_messages).to include("Pref must be other than 1")
       end
 
       it 'period_idが空では保存できない' do
         @item.period_id = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("発送までの日数 が空では保存できません")
+        expect(@item.errors.full_messages).to include("Period can't be blank")
       end
 
       it 'period_idが1では保存できない' do
         @item.period_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("発送までの日数 は1以外の値にしてください")
+        expect(@item.errors.full_messages).to include("Period must be other than 1")
       end
 
       it 'priceが空では保存できない' do
         @item.price = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("価格 が空では保存できません")
+        expect(@item.errors.full_messages).to include("Price can't be blank")
       end
 
       it 'priceが299以下だと保存できない' do
         @item.price = 299
         @item.valid?
-        expect(@item.errors.full_messages).to include("価格 は300以上9999999以下の数字で設定してください")
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
 
       it 'priceが10000000以上だと保存できない' do
         @item.price = 10000000
         @item.valid?
-        expect(@item.errors.full_messages).to include("価格 は300以上9999999以下の数字で設定してください")
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
 
       it 'priceが半角数値以外だと保存できない' do
         @item.price = "一二三"
         @item.valid?
-        expect(@item.errors.full_messages).to include("価格 は半角数値を入力してください")
+        expect(@item.errors.full_messages).to include("Price is not a number")
       end
 
       it 'userが紐付いていないと保存できない' do
         @item.user = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include('ユーザー が紐付いていないと保存できません')
+        expect(@item.errors.full_messages).to include("User must exist")
       end
     end
   end
