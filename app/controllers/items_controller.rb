2a7bfb91489fe1bcf43f70493397f2ac33de3514
saya4444 <smr.viola.38@gmail.com>
2024-02-21 20:50:23 +0900

    商品情報編集機能　実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @items = Item.order("created_at DESC")
@@ -18,17 +20,48 @@ def create
   end
 end
 
-
 def show
   @item = Item.find(params[:id])
 end
 
+def edit
+  @item = Item.find(params[:id])
+  if current_user.id != @item.user_id
+    redirect_to root_path
+  end
+end
+
+def update
+  @item = Item.find(params[:id])
+  if @item.update(item_params)
+    redirect_to item_path(@item)
+  else
+    render :edit, status: :unprocessable_entity
+  end
+end
+
+# def destroy
+#   @item = Item.find(params[:id])
+#   @item.destroy
+#   redirect_to items_path, notice: "商品が削除されました"
+# end
 
   private
 
   def item_params
     params.require(:item).permit(:image, :item, :explanation, :category_id, :condition_id, :burden_id, :pref_id, :period_id, :price).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    if current_user.id != @item.user_id
+      redirect_to root_path
+    end
+  end
+
 
 
 
